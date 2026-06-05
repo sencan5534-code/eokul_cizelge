74caa9501f397a743a894fa0a7441911db570597
sencan5534-code <sencan5534@gmail.com>
2026-06-05 17:12:01 +0300

    Revamp app title and add Excel upload feature
    Updated the app title and added functionality for uploading and processing Excel files. Users can now download the generated Excel file after processing.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,29 @@
 import streamlit as st
+import tempfile
+from excel_compressor import process_excel
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("E-Okul Excel Oluşturucu")
+
+uploaded_file = st.file_uploader(
+    "Excel dosyasını yükle",
+    type=["xlsx"]
 )
+
+if uploaded_file is not None:
+    if st.button("Excel Oluştur"):
+
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx") as temp_input:
+            temp_input.write(uploaded_file.read())
+            input_path = temp_input.name
+
+        output_path = "E_okul_template_doldurulmus.xlsx"
+
+        process_excel(input_path, output_path)
+
+        with open(output_path, "rb") as file:
+            st.download_button(
+                label="Çıktı Excel'i İndir",
+                data=file,
+                file_name="E_okul_template_doldurulmus.xlsx",
+                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            )
